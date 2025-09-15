da2148c0647d49e86db7eed6b7b2cf645200a592
Jishaan Khatri <jishaan123465@gmail.com>
2025-09-15 23:53:04 +0530

    Bubble Sort

---
+++ sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],temp=0;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int j=1;j<n;j++)
+    {
+        for(int k=0;k<n-j;k++)
+        {
+            if(a[k]>a[k+1])
+            {
+                temp=a[k];
+                a[k]=a[k+1];
+                a[k+1]=temp;
+            }
+        }
+    }
+    printf("Sorted array is:\n");
+    for(int l=0;l<n;l++)
+    {
+        printf("%d\n",a[l]);
+    }
+
+}
