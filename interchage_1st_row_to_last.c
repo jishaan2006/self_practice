c8d2fa43539a5a601450b7fcb8407f541eb2dd49
Jishaan Khatri <jishaan123465@gmail.com>
2025-09-27 18:30:09 +0530

    2D array

---
+++ interchage_1st_row_to_last.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int rows, columns;
+    printf("Enter the number of rows and columns of matrix A :\n");
+    scanf("%d %d", &rows, &columns);
+    int arr[rows][columns];
+    printf("Enter the Elements of matrix A :\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+     for (int j = 0; j < columns; j++)
+    {
+        int temp = arr[0][j];
+        arr[0][j] = arr[rows - 1][j];
+        arr[rows - 1][j] = temp;
+    }
+        printf("New matrix is :\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ matrix_symmetry.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a[3][3] =
+        {{0, 2, 3},
+         {2, 0, 6},
+         {3, 6, 7}};
+    int flag = 1;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (a[i][j] != a[j][i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+    }
+    if (flag == 1)
+        printf("symmetrical");
+    else
+        printf("not symmetrical");
+    return 0;
+}
