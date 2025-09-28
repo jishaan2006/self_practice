bfa2047ed28e54a86dc2facdfa01f0a3f948328f
Jishaan Khatri <jishaan123465@gmail.com>
2025-09-28 22:27:18 +0530

    Upper diagonal

--- upper_diagonal.c
+++ upper_diagonal.c
@@ -1,19 +1,26 @@
-#include<stdio.h>
-int main(){
-    int arr[3][3]={1,2,3,4,5,6,7,8,9};
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            printf("%d ",arr[i][j]);
+#include <stdio.h>
+int main()
+{
+    int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
     printf("Upper diagonal elements are :\n");
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(i<j){
-                printf("%d ",arr[i][j]);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i < j)
+            {
+                printf("%d ", arr[i][j]);
             }
-            else{
+            else
+            {
                 printf("  ");
             }
         }
