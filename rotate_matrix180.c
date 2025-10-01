96fa515d9f72124551c3d86932506d7cc288933f
Jishaan Khatri <jishaan123465@gmail.com>
2025-10-01 16:50:41 +0530

    Rotate matrix

--- rotate_matrix180.c
+++ rotate_matrix180.c
@@ -1,3 +1,12 @@
+/*
+input 
+1 2 3 
+4 5 6 
+7 8 9
+output 
+9 8 7 
+6 5 4
+3 2 1*/
 #include<stdio.h>
 int main(){
     int a[3][3]={1,2,3,4,5,6,7,8,9};
--- rotate_matrix90.c
+++ rotate_matrix90.c
@@ -1,3 +1,13 @@
+/*
+input 
+1 2 3
+4 5 6
+7 8 9
+output
+7 4 1 
+8 5 2
+9 6 3
+*/
 #include<stdio.h>
 int main(){
     int a[3][3]={1,2,3,4,5,6,7,8,9};
