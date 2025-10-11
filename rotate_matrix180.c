6187013fc9f98023b3e259f0dd73b64a8a10496c
Jishaan Khatri <jishaan123465@gmail.com>
2025-10-11 22:36:33 +0530

    2D array

--- rotate_matrix180.c
+++ rotate_matrix180.c
@@ -9,7 +9,13 @@ output
 3 2 1*/
 #include<stdio.h>
 int main(){
-    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    int a[3][3];
+    printf("Enter 9 Elements :\n");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
     for(int i=2;i>=0;i--){
         for(int j=2;j>=0;j--){
             printf("%d ",a[i][j]);
